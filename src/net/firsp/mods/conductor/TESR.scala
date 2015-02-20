26b6f23a9508fcf87a4f3e532088ded56c83a2c6
FirstSpring <firstspring1845@gmail.com>
2015-02-21 02:36:14 +0900

    change Rendering

--- src/net/firsp/mods/conductor/TESR.scala
+++ src/net/firsp/mods/conductor/TESR.scala
@@ -1,5 +1,6 @@
 package net.firsp.mods.conductor
 
+import cofh.api.energy.IEnergyHandler
 import net.minecraft.client.renderer.texture.TextureMap
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer
 import net.minecraft.client.renderer.{OpenGlHelper, RenderHelper, Tessellator}
@@ -8,71 +9,37 @@ import net.minecraft.util.IIcon
 import net.minecraftforge.common.util.ForgeDirection
 import net.minecraftforge.fluids.{FluidRegistry, FluidStack, IFluidHandler}
 import org.lwjgl.opengl.GL11
+import org.lwjgl.opengl.GL11._
 
 object TESR extends TileEntitySpecialRenderer {
   override def renderTileEntityAt(tile: TileEntity, x: Double, y: Double, z: Double, f: Float): Unit = {
     tile match {
       case e: TileEnergyConductor => {
-        val from = 0.5D - e.internalEnergy / 2000D
-        val to = 0.5D + e.internalEnergy / 2000D
-        GL11.glPushMatrix();
-        GL11.glTranslated(x, y, z)
-        GL11.glDisable(GL11.GL_TEXTURE_2D);
-        GL11.glDisable(GL11.GL_LIGHTING);
-        GL11.glEnable(GL11.GL_BLEND);
-        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0f, 240.0f);
-        RenderHelper.disableStandardItemLighting();
-        GL11.glDepthMask(false);
-        GL11.glDisable(GL11.GL_DEPTH_TEST);
-        // down
-        Tessellator.instance.startDrawing(GL11.GL_LINE_LOOP);
-        Tessellator.instance.setColorRGBA(255, 0, 0, 255);
-        Tessellator.instance.addVertex(from, from, from);
-        Tessellator.instance.addVertex(to, from, from);
-        Tessellator.instance.addVertex(to, from, to);
-        Tessellator.instance.addVertex(from, from, to);
-        Tessellator.instance.draw();
-        // up
-        Tessellator.instance.startDrawing(GL11.GL_LINE_LOOP);
-        Tessellator.instance.setColorRGBA(255, 0, 0, 255);
-        Tessellator.instance.addVertex(from, to, from);
-        Tessellator.instance.addVertex(to, to, from);
-        Tessellator.instance.addVertex(to, to, to);
-        Tessellator.instance.addVertex(from, to, to);
-        Tessellator.instance.draw();
-        // west
-        Tessellator.instance.startDrawing(GL11.GL_LINE_LOOP);
-        Tessellator.instance.setColorRGBA(255, 0, 0, 255);
-        Tessellator.instance.addVertex(from, from, from);
-        Tessellator.instance.addVertex(from, to, from);
-        Tessellator.instance.addVertex(from, to, to);
-        Tessellator.instance.addVertex(from, from, to);
-        Tessellator.instance.draw();
-        // east
-        Tessellator.instance.startDrawing(GL11.GL_LINE_LOOP);
-        Tessellator.instance.setColorRGBA(255, 0, 0, 255);
-        Tessellator.instance.addVertex(to, from, from);
-        Tessellator.instance.addVertex(to, to, from);
-        Tessellator.instance.addVertex(to, to, to);
-        Tessellator.instance.addVertex(to, from, to);
-        Tessellator.instance.draw();
-        // 他の描画に影響するので戻す
-        GL11.glPopMatrix();
-        RenderHelper.enableStandardItemLighting();
-        GL11.glDisable(GL11.GL_BLEND);
-        GL11.glEnable(GL11.GL_TEXTURE_2D);
-        GL11.glEnable(GL11.GL_LIGHTING);
-        GL11.glDepthMask(true);
-        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        val from = 0.5D - e.internalEnergy / 2001D
+        val to = 0.5D + e.internalEnergy / 2001D
+
+        val doRenderSide = ForgeDirection.VALID_DIRECTIONS.map(o => e.getWorldObj.getTileEntity(e.xCoord + o.offsetX, e.yCoord + o.offsetY, e.zCoord + o.offsetZ).isInstanceOf[IEnergyHandler])
+        drawBox(x, y, z, from, from, from, to, to, to, 0, 255, 255, 255)
+        //down
+        if (doRenderSide(0)) drawBox(x, y, z, from, 0.001, from, to, from, to, 0, 255, 255, 255)
+        //up
+        if (doRenderSide(1)) drawBox(x, y, z, from, to, from, to, 0.999, to, 0, 255, 255, 255)
+        //north
+        if (doRenderSide(2)) drawBox(x, y, z, from, from, 0.001, to, to, from, 0, 255, 255, 255)
+        //south
+        if (doRenderSide(3)) drawBox(x, y, z, from, from, to, to, to, 0.999, 0, 255, 255, 255)
+        //west
+        if (doRenderSide(4)) drawBox(x, y, z, 0.001, from, from, from, to, to, 0, 255, 255, 255)
+        //east
+        if (doRenderSide(5)) drawBox(x, y, z, to, from, from, 0.999, to, to, 0, 255, 255, 255)
       }
       case f: TileFluidConductor => {
         f.tank.getFluid match {
           case fs: FluidStack => {
             bindTexture(TextureMap.locationBlocksTexture)
-            val icon = try{
+            val icon = try {
               fs.getFluid.getStillIcon
-            }catch{
+            } catch {
               case _ => FluidRegistry.WATER.getStillIcon
             }
             val v = ForgeDirection.VALID_DIRECTIONS
@@ -83,17 +50,17 @@ object TESR extends TileEntitySpecialRenderer {
             //internal
             drawBox(x, y, z, from, from, from, to, to, to, internalDoRenderSide, icon)
             //down
-            if(doRenderSide(0)) drawBox(x, y, z, from, 0.001, from, to, from, to, v.map(_ != ForgeDirection.DOWN), icon)
+            if (doRenderSide(0)) drawBox(x, y, z, from, 0.001, from, to, from, to, v.map(_ != ForgeDirection.DOWN), icon)
             //up
-            if(doRenderSide(1)) drawBox(x, y, z, from, to, from, to, 0.999, to, v.map(_ != ForgeDirection.UP), icon)
+            if (doRenderSide(1)) drawBox(x, y, z, from, to, from, to, 0.999, to, v.map(_ != ForgeDirection.UP), icon)
             //north
-            if(doRenderSide(2)) drawBox(x, y, z, from, from, 0.001, to, to, from, v.map(_ != ForgeDirection.NORTH), icon)
+            if (doRenderSide(2)) drawBox(x, y, z, from, from, 0.001, to, to, from, v.map(_ != ForgeDirection.NORTH), icon)
             //south
-            if(doRenderSide(3)) drawBox(x, y, z, from, from, to, to, to, 0.999, v.map(_ != ForgeDirection.SOUTH), icon)
+            if (doRenderSide(3)) drawBox(x, y, z, from, from, to, to, to, 0.999, v.map(_ != ForgeDirection.SOUTH), icon)
             //west
-            if(doRenderSide(4)) drawBox(x, y, z, 0.001, from, from, from, to, to, v.map(_ != ForgeDirection.WEST), icon)
+            if (doRenderSide(4)) drawBox(x, y, z, 0.001, from, from, from, to, to, v.map(_ != ForgeDirection.WEST), icon)
             //east
-            if(doRenderSide(5)) drawBox(x, y, z, to, from, from, 0.999, to, to, v.map(_ != ForgeDirection.EAST), icon)
+            if (doRenderSide(5)) drawBox(x, y, z, to, from, from, 0.999, to, to, v.map(_ != ForgeDirection.EAST), icon)
           }
           case _ =>
         }
@@ -102,7 +69,7 @@ object TESR extends TileEntitySpecialRenderer {
   }
 
   def drawBox(x: Double, y: Double, z: Double, fromX: Double, fromY: Double, fromZ: Double, toX: Double, toY: Double, toZ: Double, doRenderSide: Array[Boolean], icon: IIcon) = {
-    import org.lwjgl.opengl.GL11._
+
     val t = Tessellator.instance
     glPushMatrix
     glPushAttrib(GL_ENABLE_BIT)
@@ -172,6 +139,79 @@ object TESR extends TileEntitySpecialRenderer {
     glDisable(GL_BLEND)
     glDepthMask(true)
     glPopAttrib
+    glPopMatrix
+  }
+
+  def drawBox(x: Double, y: Double, z: Double, fromX: Double, fromY: Double, fromZ: Double, toX: Double, toY: Double, toZ: Double, r: Int, g: Int, b: Int, a: Int) = {
+    val t = Tessellator.instance
+    glPushMatrix
+    glTranslated(x, y, z)
+    glDisable(GL_TEXTURE_2D)
+    glDisable(GL_LIGHTING)
+    glDisable(GL_CULL_FACE)
+    OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0f, 240.0f)
+
+    t.setColorRGBA(r, g, b, a)
+    //down
+    t.startDrawingQuads
+    t.setColorRGBA(r, g, b, a)
+    t.addVertex(fromX, fromY, fromZ)
+    t.addVertex(toX, fromY, fromZ)
+    t.addVertex(toX, fromY, toZ)
+    t.addVertex(fromX, fromY, toZ)
+    t.draw
+    //up
+    /*t.startDrawingQuads
+    t.setColorRGBA(r, g, b, a)
+    t.addVertex(fromX, toY, fromZ)
+    t.addVertex(toX, toY, fromZ)
+    t.addVertex(toX, toY, toZ)
+    t.addVertex(fromX, toY, toZ)
+    t.draw*/
+    glBegin(GL_QUADS)
+    glColor3d(0, 1, 1)
+    glVertex3d(fromX, toY, fromZ)
+    glVertex3d(toX, toY, fromZ)
+    glVertex3d(toX, toY, toZ)
+    glVertex3d(fromX, toY, toZ)
+    glEnd
+    //north
+    t.startDrawingQuads
+    t.setColorRGBA(r, g, b, a)
+    t.addVertex(fromX, fromY, fromZ)
+    t.addVertex(fromX, toY, fromZ)
+    t.addVertex(toX, toY, fromZ)
+    t.addVertex(toX, fromY, fromZ)
+    t.draw
+    //south
+    t.startDrawingQuads
+    t.setColorRGBA(r, g, b, a)
+    t.addVertex(fromX, fromY, toZ)
+    t.addVertex(fromX, toY, toZ)
+    t.addVertex(toX, toY, toZ)
+    t.addVertex(toX, fromY, toZ)
+    t.draw
+    //west
+    t.startDrawingQuads
+    t.setColorRGBA(r, g, b, a)
+    t.addVertex(fromX, fromY, fromZ)
+    t.addVertex(fromX, toY, fromZ)
+    t.addVertex(fromX, toY, toZ)
+    t.addVertex(fromX, fromY, toZ)
+    t.draw
+
+    //east
+    t.startDrawingQuads
+    t.setColorRGBA(r, g, b, a)
+    t.addVertex(toX, fromY, fromZ)
+    t.addVertex(toX, toY, fromZ)
+    t.addVertex(toX, toY, toZ)
+    t.addVertex(toX, fromY, toZ)
+    t.draw
+
+    glEnable(GL_TEXTURE_2D)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_CULL_FACE)
     glPopMatrix
   }
 }
